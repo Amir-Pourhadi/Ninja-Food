384a11c1a145924837cd5ec170a09c99a6f9aa5f
Amir Pourhadi <AmirPourhadi@outlook.com>
2021-12-03 16:08:23 +0330

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#ff6363",
+        secondary: {
+          100: "#e2e2d5",
+          200: "#888883",
+        },
+      },
+    },
   },
   variants: {},
   plugins: [],
-}
+};
